6d8522fa97130fc0c91e04d46a8c7d5ca16b7e3e
christynicholas-co <ms.christynicholas@outlook.com>
2025-05-15 13:36:31 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Bytes&Brushes</h1>
+      <h2>Currently editing, come back often to see some magic happen!</h2>
+    </div>
+  );
+}
